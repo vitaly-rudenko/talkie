005c55aeabdac970cd6e56e26e549ba9ba6c06a3
Vitaly Rudenko <maurice.dederick@gmail.com>
2026-05-17 14:10:20 +0300

    Use spawn instead of exec

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
-import { exec } from 'node:child_process'
-import { readFileSync } from 'node:fs'
-import { mkdir, readdir, rm, stat } from 'node:fs/promises'
+import { spawn } from 'node:child_process'
+import { mkdir, open, readdir, readFile, rm, stat } from 'node:fs/promises'
 import { homedir } from 'node:os'
 import { join } from 'node:path'
-import { promisify } from 'node:util'
 
-const execAsync = promisify(exec)
 const exitIfEmpty = async (path: string) => {
   const stats = await stat(path).catch(() => null)
   if (!stats || stats.size === 0) process.exit(0)
 }
 
 const MAX_OLD_OPERATIONS = 5
-const WHISPER_MODEL_PATH = join(homedir(), 'ggml-large-v3-turbo-q5_0.bin')
-const WHISPER_VAD_MODEL_PATH = join(homedir(), 'ggml-silero-v6.2.0.bin')
+const WHISPER_MODEL_PATH = process.env.WHISPER_MODEL_PATH || join(homedir(), 'ggml-large-v3-turbo-q5_0.bin')
+const WHISPER_VAD_MODEL_PATH = process.env.WHISPER_VAD_MODEL_PATH || join(homedir(), 'ggml-silero-v6.2.0.bin')
 
 // YYYY-MM-DD-HH-mm-SS
 const operationId = new Date().toISOString().slice(0, 19).replaceAll(/\D/g, '-')
@@ -27,36 +24,44 @@ for (const operationId of operationIds.sort().slice(0, -MAX_OLD_OPERATIONS)) {
 
 // Sox
 const recordingPath = join(operationPath, 'recording.wav')
-const sox = execAsync(
-  `sox \
-    --default-device \
-    --rate 16000 \
-    --channels 1 \
-    --type wav \
-    "${recordingPath}"`,
+const sox = spawn(
+  'sox',
+  [
+    //
+    '--default-device',
+    ['--rate', '16000'],
+    ['--channels', '1'],
+    ['--type', 'wav'],
+    recordingPath,
+  ].flat(),
 )
-process.once('SIGINT', () => sox.child.kill('SIGINT'))
-await sox.catch(() => {})
+process.once('SIGINT', () => sox.kill('SIGINT'))
+await new Promise(r => sox.on('exit', r))
 await exitIfEmpty(recordingPath)
 
 // Whisper
 const transcriptionPath = join(operationPath, 'transcription.txt')
-const whisper = execAsync(
-  `whisper-cli \
-    --model "${WHISPER_MODEL_PATH}" \
-    --file "${recordingPath}" \
-    --no-prints \
-    --no-timestamps \
-    --language auto \
-    -sns \
-    --vad \
-    -vm "${WHISPER_VAD_MODEL_PATH}" \
-    > "${transcriptionPath}"`,
+const transcriptionFileHandle = await open(transcriptionPath, 'w')
+const whisper = spawn(
+  'whisper-cli',
+  [
+    //
+    ['--model', WHISPER_MODEL_PATH],
+    ['--file', recordingPath],
+    '--no-prints',
+    '--no-timestamps',
+    ['--language', 'auto'],
+    '-sns',
+    '--vad',
+    ['-vm', WHISPER_VAD_MODEL_PATH],
+  ].flat(),
+  { stdio: ['ignore', transcriptionFileHandle.fd, 'ignore'] },
 )
-process.once('SIGINT', () => whisper.child.kill('SIGINT'))
-await whisper.catch(() => {})
+process.once('SIGINT', () => whisper.kill('SIGINT'))
+await new Promise(r => whisper.on('exit', r))
+await transcriptionFileHandle.close()
 await exitIfEmpty(transcriptionPath)
 
-console.log(readFileSync(transcriptionPath, 'utf8').trim())
+console.log((await readFile(transcriptionPath, 'utf8')).trim())
 
 export {}
